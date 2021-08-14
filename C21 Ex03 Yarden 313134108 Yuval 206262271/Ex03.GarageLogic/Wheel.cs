f42cdae9286e1df7118a30d3d9b54760e817ee04
Yarden Naim <yardenyaakovna@mta.ac.il>
2021-08-14 14:20:41 +0300

    Wheel class

---
+++ C21 Ex03 Yarden 313134108 Yuval 206262271/Ex03.GarageLogic/Wheel.cs
@@ -0,0 +1,58 @@
+﻿namespace Ex03.GarageLogic
+{
+    public sealed class Wheel
+    {
+        private string m_ManufacturerName;
+        private float m_CurrentAirPressure;
+        private readonly float r_MaxAirPressure;
+
+        public Wheel(string i_ManufacturerName, float i_CurrentAirPressure, float i_MaxAirPressure)
+        {
+            this.m_ManufacturerName = i_ManufacturerName;
+            this.m_CurrentAirPressure = i_CurrentAirPressure;
+            this.r_MaxAirPressure = i_MaxAirPressure;
+        }
+
+        public float CurrentAirPressure
+        {
+            get
+            {
+                return m_CurrentAirPressure;
+            }
+
+            set
+            {
+                m_CurrentAirPressure = value;
+            }
+        }
+
+        public string ManufacturerName
+        {
+            get
+            {
+                return m_ManufacturerName;
+            }
+
+            set
+            {
+                m_ManufacturerName = value;
+            }
+        }
+
+        public void FillAirPressureToMax()
+        {
+            this.m_CurrentAirPressure = this.r_MaxAirPressure;
+        }
+
+        public override string ToString()
+        {
+            return string.Format(
+                @"Wheels Manufacturer - {0}
+Wheels PSI - {1} out of {2}
+",
+                this.m_ManufacturerName,
+                this.m_CurrentAirPressure,
+                this.r_MaxAirPressure);
+        }
+    }
+}
